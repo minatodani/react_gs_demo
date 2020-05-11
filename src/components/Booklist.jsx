62fcfdcc1fa646343e2bb9d37138f519f32906aa
minatodani <cougug@gmail.com>
2020-05-11 20:46:58 +0900

    first commit

---
+++ src/components/Booklist.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+const Booklist = (props) => {
+  const [bookData, setBookData] = useState(null);
+  useEffect(() => {
+    const result = props
+      .getData?.(props.language)
+      .then((response) => setBookData(response));
+  }, [props]);
+  console.log(bookData);
+  //   console.log(bookData.data.items.volumeInfo.imageLinks.thumbnail);
+  const imageStyle = { height: 100, width: 100 };
+
+  return (
+    <div>
+      <ul>
+        {bookData === null ? (
+          <p>now loading...</p>
+        ) : (
+          bookData.data.items.map((x, index) => (
+            <div key={index}>
+              <li>{x.volumeInfo.title}</li>
+              <img
+                src={x.volumeInfo.imageLinks.thumbnail}
+                alt=""
+                style={imageStyle}
+              />
+            </div>
+          ))
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Booklist;
